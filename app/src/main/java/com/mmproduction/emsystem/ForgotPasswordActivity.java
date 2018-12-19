e63315c3581890b82aa94e8cc0600e0d479df46d
Param <parammoradiya98@gmail.com>
2018-12-19 20:32:52 +0530

    ok all project here

--- app/src/main/java/com/mmproduction/emsystem/ForgotPasswordActivity.java
+++ app/src/main/java/com/mmproduction/emsystem/ForgotPasswordActivity.java
@@ -3,6 +3,7 @@ package com.mmproduction.emsystem;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -16,18 +17,19 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class ForgotPasswordActivity extends AppCompatActivity {
-  FirebaseAuth firebaseAuth;
-  Button reset;
-  EditText change;
+    FirebaseAuth firebaseAuth;
+    Button reset;
+    EditText change;
     private Toolbar mtoolbar;
 
     private ProgressDialog mRegprogress;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forgot);
         change = (EditText) findViewById(R.id.reset_pass);
-        reset = (Button)findViewById(R.id.reset_btn);
+        reset = (Button) findViewById(R.id.reset_btn);
         firebaseAuth = FirebaseAuth.getInstance();
 
         mtoolbar = (Toolbar) findViewById(R.id.Register_toolbar);
@@ -43,34 +45,35 @@ public class ForgotPasswordActivity extends AppCompatActivity {
             public void onClick(View view) {
 
 
-               String email = change.getText().toString();
+                String email = change.getText().toString();
 
                 mRegprogress.setMessage("sendind reset link to this E-mail");
                 mRegprogress.show();
-               if(email.equals(""))
-               {
-                   mRegprogress.dismiss();
-                   Toast.makeText(ForgotPasswordActivity.this,"Email is required...",Toast.LENGTH_SHORT).show();
-               }
-               else
-               {
-                   firebaseAuth.sendPasswordResetEmail(email).addOnCompleteListener(new OnCompleteListener<Void>() {
-                       @Override
-                       public void onComplete(@NonNull Task<Void> task) {
-                           if(task.isSuccessful())
-                           {
-                               mRegprogress.dismiss();
-                               Toast.makeText(ForgotPasswordActivity.this,"Password Reset Link send to your mail",Toast.LENGTH_SHORT).show();
-                               finish();
-                               startActivity(new Intent(ForgotPasswordActivity.this,LoginActivity.class));
-                           }
-                           else {
-                               mRegprogress.dismiss();
-                               Toast.makeText(ForgotPasswordActivity.this,"Error in sending mail link...",Toast.LENGTH_SHORT).show();
-                           }
-                       }
-                   });
-               }
+                if (email.equals("")) {
+                    mRegprogress.dismiss();
+                    Toast.makeText(ForgotPasswordActivity.this, "Email is required...", Toast.LENGTH_SHORT).show();
+                } else {
+                    if (CheckNetwork.isInternetAvailable(ForgotPasswordActivity.this)) {
+                        firebaseAuth.sendPasswordResetEmail(email).addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                if (task.isSuccessful()) {
+                                    mRegprogress.dismiss();
+                                    Toast.makeText(ForgotPasswordActivity.this, "Password Reset Link send to your mail", Toast.LENGTH_SHORT).show();
+                                    finish();
+                                    startActivity(new Intent(ForgotPasswordActivity.this, LoginActivity.class));
+                                } else {
+                                    mRegprogress.dismiss();
+                                    Toast.makeText(ForgotPasswordActivity.this, "Error in sending mail link...", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+                    }else{
+                        mRegprogress.dismiss();
+                        Snackbar snackbar = Snackbar.make(reset, "No Internet Connection", Snackbar.LENGTH_LONG);
+                        snackbar.show();
+                    }
+                }
             }
         });
     }
