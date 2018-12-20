63b5bc0c6372c8ec4a21859669f9b02746048883
Param <parammoradiya98@gmail.com>
2018-12-20 10:51:34 +0530

    ok all project here

--- app/src/main/java/com/mmproduction/emsystem/ReportActivity.java
+++ app/src/main/java/com/mmproduction/emsystem/ReportActivity.java
@@ -89,6 +89,7 @@ public class ReportActivity extends AppCompatActivity {
         getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_HIDDEN);
 
 
+        //this is report activity
         //mprogressbar = (ProgressBar)findViewById(R.id.report_progress_bar);
         mbtncooseimag = (Button) findViewById(R.id.btnChooseimage);
         mbtnuploadimage = (Button) findViewById(R.id.btnUploagImage);
