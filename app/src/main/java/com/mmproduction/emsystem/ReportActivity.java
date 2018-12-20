c0d12de006fa1701cd360b460f8b543219d4398f
akashgolakiya <46000351+akashgolakiya@users.noreply.github.com>
2018-12-20 11:42:47 +0530

    reportActivity
    comment

--- app/src/main/java/com/mmproduction/emsystem/ReportActivity.java
+++ app/src/main/java/com/mmproduction/emsystem/ReportActivity.java
@@ -57,7 +57,7 @@ public class ReportActivity extends AppCompatActivity {
 
     private static final String DATE_PATTERN =
             "(0?[1-9]|1[012]) [/.-] (0?[1-9]|[12][0-9]|3[01]) [/.-] ((19|20)\\d\\d)";
-
+//report
     ProgressBar mprogressbar;
     ImageView mImage;
     Button mbtncooseimag, mbtnuploadimage;
